5dc6b8df51d46543a9412dbf1a262ea9409e26b3
Artist-30 <socialartist30@gmail.com>
2025-08-03 02:07:50 +0530

    add CircleAvatar practise file

---
+++ lib/Lec_44_CircleAvatar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Lec44 extends StatelessWidget {
+  const Lec44({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFE0E0E0),
+        title: Text("ListTile"),
+      ),
+      body: Center(
+        child: CircleAvatar(
+          child: Container(
+            width: 60,
+            height: 60,
+            child: Column(
+              children: [
+                Container(
+                  width: 33,
+                  height: 33,
+                  child: Image.asset(
+                    "assets/images/iron_man.png",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Text(
+                  "Name",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: Colors.green,
+          maxRadius: 60,
+        ),
+      ),
+    );
+  }
+}
